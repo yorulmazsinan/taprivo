b7f82446047c7b1f1683038a29af6216e09b4ef6
Sinan Yorulmaz <info@sinanyorulmaz.com>
2026-09-09 11:53:46 +0300

    docs: prepare 0.1.0b2 changelog

--- tests/unit/test_version.py
+++ tests/unit/test_version.py
@@ -4,5 +4,5 @@ import taprivo
 
 
 def test_version_matches_package_metadata() -> None:
-    assert taprivo.__version__ == "0.1.0b1"
-    assert version("taprivo") == "0.1.0b1"
+    assert taprivo.__version__ == "0.1.0b2"
+    assert version("taprivo") == "0.1.0b2"
